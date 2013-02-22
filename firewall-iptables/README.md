8fec1b823bc69cdebdd548bf9aded783aeefa706
Thomas VIAL <github@ifusio.com>
2013-02-22 14:26:20 +0100

    Refactored and explained. Now ready o use.

---
+++ firewall-iptables/README.md
@@ -0,0 +1,58 @@
+# Firewall iptables
+
+Ruby script to manage a linux firewall based on `iptables`
+
+Some defaults :
+- Incoming & Forwarded trafic is dropped
+- Outgoing trafic is accepted
+
+Don't forget to adapt this script to your needs before you apply the rules.
+
+## Setup
+
+Path to iptables
+
+    IPTABLES   = "/sbin/iptables"
+
+Interface identifier
+
+    IFACE_NAME = "eth0"
+
+Current server interface IP
+
+    IFACE_IP   = "192.168.1.200"
+
+Current server network
+
+    NETWORK    = "192.168.1.0/24"
+
+## Firewall rules
+
+TCP ports to open
+
+    ipt_input_ports_tcp       = [22, 25, 53, 80, 113, 143, 6667]
+
+UDP ports to open
+
+    ipt_input_ports_udp       = [53, 113]
+
+Protocols ID to open
+
+    ipt_input_ports_protocol  = [47]
+
+Port forwarding 
+
+    ipt_forward_port          = {
+                                  # remote_ip => { local_port => remote_port }
+                                  "192.168.1.201" => { 201 => 22, 8080 => 80 }
+                                }
+
+## Usage
+
+Test the rules (nothing will be applied)
+
+    ./firewall.rb test
+
+Apply the rules
+
+    ./firewall.rb apply
---
+++ firewall-iptables/firewall-iptables.rb
@@ -0,0 +1,152 @@
+#!/usr/bin/ruby
+#
+# Name:     firewall.rb
+# Author:   Thomas VIAL
+# Usage:    ./firewall-iptables.rb (test|apply)
+#           => 'test' will only print the rules
+#           => 'apply' will execute iptables and apply the rules
+
+
+# Configuration      
+IPTABLES   = "/sbin/iptables"
+IFACE_NAME = "eth0"
+IFACE_IP   = "192.168.1.200"
+NETWORK    = "192.168.1.0/24"
+
+# Firewall settings
+ipt_input_ports_tcp       = [22, 25, 53, 80, 113, 143, 6667]
+ipt_input_ports_udp       = [53, 113]
+ipt_input_ports_protocol  = [47]
+ipt_forward_port          = {
+                              "192.168.1.201" => { 201 => 22, 8080 => 80 }
+                            }
+
+#
+#
+# /!\ # Don't touch the next part
+#
+#
+
+def arg
+  ARGV[0].to_s
+end
+
+def notify_and_exit
+  puts "----------------------------------------------------------------"
+  puts "Error : Wrong or Missing parameter"
+  puts "Use './firewall-iptables.rb test' to check rules before apply" 
+  puts "Use './firewall-iptables.rb apply' to apply rules set" 
+  puts "----------------------------------------------------------------"
+  exit
+end
+
+if arg.empty?
+  notify_and_exit
+elsif not ["test", "apply"].include?(arg)
+  notify_and_exit  
+end
+
+def label(label_text)
+  if arg.eql? "test"
+    puts(label_text)
+  elsif arg.eql? "apply"
+    print(label_text)
+  end
+end
+
+def cmd(cmd_options)
+  if arg.eql? "test"
+    puts(cmd_options)
+  elsif arg.eql? "apply"
+    system(cmd_options)
+  end
+end
+
+def confirm(confirm_text)
+    if arg.eql? "test"
+    puts ""
+  elsif arg.eql? "apply"
+    puts(confirm_text)
+  end
+end
+
+puts "----------------------------------------------------------------"
+puts ""
+puts "                      FIREWALL IPTABLES"
+puts ""
+puts "----------------------------------------------------------------"
+puts ""
+
+label("> Enabling proc                                          ")
+proc_files = [
+  "/proc/sys/net/ipv4/tcp_syncookies",
+  "/proc/sys/net/ipv4/ip_forward"
+]
+proc_files.each do |f|
+  if File.exists?(f)
+    p = File.open(f, "w")
+    p.write("1")
+    p.close   
+  end
+end
+confirm("[ OK ]")
+
+label("> Flush existing chains                                 ")
+ipt_flush   = ['INPUT', 'OUTPUT', 'FORWARD']
+ipt_flush.each do |f|
+  cmd("sudo #{IPTABLES} -F #{f}")
+end
+confirm("[ OK ]")
+
+label("> Setting DROP policy                                   ")
+ipt_policy   = ['INPUT', 'FORWARD']
+ipt_policy.each do |f|
+  cmd("sudo #{IPTABLES} -P #{f} DROP")
+end
+confirm("[ OK ]")
+
+label("> Setting Up NAT                                        ")
+cmd("sudo #{IPTABLES} -F -t nat")
+cmd("sudo #{IPTABLES} -P FORWARD DROP")
+cmd("sudo #{IPTABLES} -A FORWARD -i #{IFACE_NAME} -j ACCEPT")
+cmd("sudo #{IPTABLES} -A INPUT -i #{IFACE_NAME} -j ACCEPT")
+cmd("sudo #{IPTABLES} -A OUTPUT -o #{IFACE_NAME} -j ACCEPT")
+confirm("[ OK ]")
+
+label("> Allow ICMP & loopback packets                          ")
+cmd("sudo #{IPTABLES} -A INPUT -p icmp -j ACCEPT")
+cmd("sudo #{IPTABLES} -A INPUT -i lo -j ACCEPT")
+cmd("sudo #{IPTABLES} -A OUTPUT -o lo -j ACCEPT")
+confirm("[ OK ]")
+
+
+label("> Opening ports via TCP                                  ")
+ipt_input_ports_tcp.each do |f|
+  cmd("sudo #{IPTABLES} -A INPUT -p tcp --dport #{f} -j ACCEPT")
+end
+confirm("[ OK ]")
+
+label("> Opening ports via UDP                                 ")
+ipt_input_ports_udp.each do |f|
+  cmd("sudo #{IPTABLES} -A INPUT -p udp --dport #{f} -j ACCEPT")
+end
+confirm("[ OK ]")
+
+label("> Opening ports via Protocols ID                         ")
+ipt_input_ports_protocol.each do |f|
+  cmd("sudo #{IPTABLES} -A INPUT -p #{f} -d #{NETWORK} -j ACCEPT")
+end
+confirm("[ OK ]")
+
+ipt_forward_port.each do |ip,arr|
+  label("> Setting up port forwarding to #{ip}  ")
+  arr.each do |from,to|
+    cmd("sudo #{IPTABLES} -A FORWARD -p tcp --dport #{from} -j ACCEPT")
+    cmd("sudo #{IPTABLES} -t nat -A PREROUTING -i #{IFACE_NAME} -p tcp --dport #{from} -j DNAT --to #{ip}:#{to}")
+    cmd("sudo #{IPTABLES} -t nat -A POSTROUTING -p tcp --dport #{to} -j SNAT --to #{IFACE_IP}") 
+  end
+end
+confirm("[ OK ]")
+
+puts ""
+puts "----------------------------------------------------------------"
